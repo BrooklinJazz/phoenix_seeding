04123f3270e36535bf6b8713508f054585bc3484
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-06-22 21:30:35 -0400

    reusable posts test fixture

--- test/seed/posts_test.exs
+++ test/seed/posts_test.exs
@@ -2,22 +2,10 @@ defmodule Seed.PostsTest do
   use Seed.DataCase
 
   alias Seed.Posts
+  use Seed.PostsFixture
 
   describe "posts" do
     alias Seed.Posts.Post
-
-    @valid_attrs %{content: "some content", title: "some title"}
-    @update_attrs %{content: "some updated content", title: "some updated title"}
-    @invalid_attrs %{content: nil, title: nil}
-
-    def post_fixture(attrs \\ %{}) do
-      {:ok, post} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Posts.create_post()
-
-      post
-    end
 
     test "list_posts/0 returns all posts" do
       post = post_fixture()
---
+++ test/support/fixtures/post_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Seed.PostsFixture do
+  alias Seed.Posts
+
+  defmacro __using__([]) do
+    quote do
+      @valid_attrs %{content: "some content", title: "some title"}
+      @update_attrs %{content: "some updated content", title: "some updated title"}
+      @invalid_attrs %{content: nil, title: nil}
+
+      def post_fixture(attrs \\ %{}) do
+        {:ok, post} =
+          attrs
+          |> Enum.into(@valid_attrs)
+          |> Posts.create_post()
+
+        post
+      end
+    end
+  end
+end
